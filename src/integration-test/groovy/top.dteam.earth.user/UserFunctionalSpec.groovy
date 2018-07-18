6882c055f67d049a351428397dd7a88c8e4fac49
foxgem <jianhgreat@hotmail.com>
2018-07-18 11:49:41 +0800

    add functional test for user password encryption.

---
+++ src/integration-test/groovy/top.dteam.earth.user/UserFunctionalSpec.groovy
@@ -0,0 +1,34 @@
+package top.dteam.earth.user
+
+import grails.gorm.transactions.Rollback
+import grails.testing.mixin.integration.Integration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import spock.lang.Specification
+import top.dteam.earth.backend.user.User
+
+@Integration
+@Rollback
+class UserFunctionalSpec extends Specification {
+
+    void "password在持久化后需要加密"() {
+        setup:
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder()
+
+        when: 'insert'
+        User user = new User(username: 13572211111, password: '123456', displayName: 'user')
+                .save(flush: true)
+
+        then:
+        user.password != '123456'
+        encoder.matches('123456', user.password)
+
+        when: 'update'
+        user.password = 'abcdef'
+        user.save(flush: true)
+
+        then:
+        user.password != 'abcdef'
+        encoder.matches('abcdef', user.password)
+    }
+
+}
