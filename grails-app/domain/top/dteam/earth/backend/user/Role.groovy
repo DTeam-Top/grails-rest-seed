035d1349c76dfe6b4e8b849d8860b220feeea027
foxgem <jianhgreat@hotmail.com>
2018-08-20 16:11:11 +0800

    remove unused role list

--- grails-app/domain/top/dteam/earth/backend/user/Role.groovy
+++ grails-app/domain/top/dteam/earth/backend/user/Role.groovy
@@ -14,26 +14,12 @@ class Role implements Serializable {
     String authority
 
     static constraints = {
-        authority nullable: false, blank: false, unique: true, inList: validRoles()
+        authority nullable: false, blank: false, unique: true
     }
 
     static mapping = {
         cache true
         comment '角色'
         authority comment: '角色名'
-    }
-
-    static List validRoles() {
-        [
-                'ROLE_ADMIN', // 管理员
-                'ROLE_YY', // 运营
-                'ROLE_KF', // 客服
-                'ROLE_CW', // 财务
-                'ROLE_GYS', // 供应商
-                'ROLE_YJDL', // 一级代理
-                'ROLE_EJDL', // 二级代理
-                'ROLE_EJDLYG', // 二级代理员工
-                'ROLE_YH' // 用户
-        ]
     }
 }
