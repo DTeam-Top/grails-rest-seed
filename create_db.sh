fa767f603b29ff5f8bfac54fb124ba6917cb518c
Feng Yu <abcfy2@users.noreply.github.com>
2018-09-06 17:05:56 +0800

    Update create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -1,7 +1,5 @@
 sudo -u postgres psql <<EOF
 create user $1_admin with password 'admin';
-create database $1;
-alter DATABASE $1 owner to $1_admin;
-create database $1_test;
-alter DATABASE $1_test owner to $1_admin;
+create database $1 owner $1_admin;
+create database $1_test owner $1_admin;
 EOF
