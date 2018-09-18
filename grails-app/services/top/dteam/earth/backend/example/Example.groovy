93c19dae9462e6a91c63a5fdda2f87345f7debb4
foxgem <jianhgreat@hotmail.com>
2018-09-18 12:24:21 +0800

    fix compile error caused by Example.groovy without correct imports.

--- grails-app/services/top/dteam/earth/backend/example/Example.groovy
+++ grails-app/services/top/dteam/earth/backend/example/Example.groovy
@@ -1,5 +1,9 @@
 package top.dteam.earth.backend.example
 
+import grails.databinding.converters.ValueConverter
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+
 import java.time.LocalDateTime
 
 class Example {
