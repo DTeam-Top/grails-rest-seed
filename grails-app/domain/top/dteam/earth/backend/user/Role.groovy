c03e13b368da0eb642e4f205641418aa74d9e554
foxgem <jianhgreat@hotmail.com>
2018-12-07 11:40:23 +0800

    add inList in Role.

--- grails-app/domain/top/dteam/earth/backend/user/Role.groovy
+++ grails-app/domain/top/dteam/earth/backend/user/Role.groovy
@@ -14,7 +14,7 @@ class Role implements Serializable {
     String authority
 
     static constraints = {
-        authority nullable: false, blank: false, unique: true
+        authority nullable: false, blank: false, unique: true, inList: validRoles()
     }
 
     static mapping = {
