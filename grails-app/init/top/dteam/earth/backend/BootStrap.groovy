656df9df2599cb5e2138b1b5f3032d585b60f86f
foxgem <jianhgreat@hotmail.com>
2018-12-17 16:18:48 +0800

    env development enabled.

--- grails-app/init/top/dteam/earth/backend/BootStrap.groovy
+++ grails-app/init/top/dteam/earth/backend/BootStrap.groovy
@@ -2,14 +2,36 @@ package top.dteam.earth.backend
 
 import groovy.util.logging.Slf4j
 import org.springframework.boot.info.GitProperties
+import top.dteam.earth.backend.user.Role
+import top.dteam.earth.backend.user.User
+import top.dteam.earth.backend.user.UserService
 
 @Slf4j
 class BootStrap {
+
     GitProperties gitProperties
+    UserService userService
 
     def init = { servletContext ->
-        log.info("Application running at commit: ${gitProperties.shortCommitId}, branch: ${gitProperties.branch}, commit time: ${gitProperties.commitTime}, build time: ${gitProperties.getDate('build.time')}")
+        log.info("Application running at commit: {}, branch: {}, commit time: {}, build time: {}"
+                , gitProperties.shortCommitId
+                , gitProperties.branch
+                , gitProperties.commitTime
+                , gitProperties.getDate('build.time'))
+
+        environments {
+            development {
+                Role.validRoles().each {
+                    new Role(authority: it).save()
+                }
+                User user = new User(username: '11111111111', password: 'admin', displayName: 'admin')
+                userService.createUserWithRole(user, 'ROLE_ADMIN')
+            }
+        }
+
     }
+
     def destroy = {
     }
+
 }
